69931e17a85f471900783bac56498507e0a35996
Kris Jordan <kris@cs.unc.edu>
2020-04-18 15:31:03 +0000

    Improve scanner-main.c demo file

--- include/Parser.h
+++ include/Parser.h
@@ -5,9 +5,10 @@
 #include "Node.h"
 
 /**
- * Given a Scanner, return a parse tree Node* from it.
- * Caller is responsible for calling Node_drop on returned
- * pointer.
+ * Given a Scanner, returns a pointer to the `Node` at
+ * the root of the parse tree. The caller of `parse`
+ * owns the resulting `Node*` and is responsible for
+ * calling `Node_drop` to free its allocated memory.
  */
 Node* parse(Scanner *s);
 
--- src/scanner-main.c
+++ src/scanner-main.c
@@ -7,54 +7,62 @@
 
 #define BUFF_SIZE 80 
 
-#define CHILD 0
-
 /**
  * This program reads an input line from stdin and prints textual
- * representations of the tokens read in as inputs.
+ * representations of the tokens scanned from lines of input.
  */
 
-size_t read_line(FILE *stream, Str *line);
+// These three functions provide the basis of a REPL:
+// Read-Evaluate-Print-Loop
+size_t read(Str *line, FILE *stream);
+Scanner eval(Str *input);
+void print(Scanner scanner);
 
 int main()
 {
-    Str line;
-    size_t bytes;
-    do {
-        printf("scanner> ");
-
-        bytes = read_line(stdin, &line);
-
-        Scanner scanner = Scanner_value(CharItr_of_Str(&line));
-        while (Scanner_has_next(&scanner)) {
-            Token next = Scanner_next(&scanner);
-            switch (next.type) {
-                case END_TOKEN: 
-                    printf("END\n");
-                    break;
-                case WORD_TOKEN:
-                    printf("WORD(%s)\n", Str_cstr(&next.lexeme));
-                    break;
-                case PIPE_TOKEN:
-                    printf("PIPE\n");
-                    break;
-            }
-            Str_drop(&next.lexeme);
-        }
-        Str_drop(&line);
-
-    } while (bytes > 0);
+    Str line = Str_value(BUFF_SIZE);
+    while (read(&line, stdin)) {
+        print(eval(&line));
+    }
+    Str_drop(&line);
     return EXIT_SUCCESS;
 }
 
-size_t read_line(FILE *stream, Str *line) {
+size_t read(Str *line, FILE *stream) {
+    printf("scanner> ");
+
+    // Clear Str contents.
+    Str_splice(line, 0, Str_length(line), NULL, 0);
+
     static char buffer[BUFF_SIZE];
-    *line = Str_value(BUFF_SIZE);
     while (fgets(buffer, BUFF_SIZE, stream) != NULL) {
         Str_append(line, buffer);
         if (strchr(buffer, '\n') != NULL) {
             break;
         }
     }
+
     return Str_length(line);
+}
+
+Scanner eval(Str *line) {
+    return Scanner_value(CharItr_of_Str(line));
+}
+
+void print(Scanner scanner) {
+    while (Scanner_has_next(&scanner)) {
+        Token next = Scanner_next(&scanner);
+        switch (next.type) {
+            case END_TOKEN: 
+                printf("END\n");
+                break;
+            case WORD_TOKEN:
+                printf("WORD(%s)\n", Str_cstr(&next.lexeme));
+                break;
+            case PIPE_TOKEN:
+                printf("PIPE\n");
+                break;
+        }
+        Str_drop(&next.lexeme);
+    }
 }
