4fc303c01156ccd1df1648435c79bf2a000ba530
Kris Jordan <kris@cs.unc.edu>
2020-04-18 05:22:59 +0000

    Add Node

---
+++ src/Node.c
@@ -0,0 +1,36 @@
+#include "Node.h"
+#include "Guards.h"
+
+Node* ErrorNode_new(const char *msg)
+{
+    Node *node = malloc(sizeof(Node));
+    OOM_GUARD(node, __FILE__, __LINE__);
+    node->type = ERROR_NODE;
+    node->data.error = msg;
+    return node;
+}
+
+Node* CommandNode_new(StrVec words)
+{
+    Node *node = malloc(sizeof(Node));
+    OOM_GUARD(node, __FILE__, __LINE__);
+    node->type = COMMAND_NODE;
+    node->data.command.words = words;
+    return node;
+}
+
+Node* PipeNode_new(Node *left, Node *right)
+{
+    Node *node = malloc(sizeof(Node));
+    OOM_GUARD(node, __FILE__, __LINE__);
+    node->type = PIPE_NODE;
+    node->data.pipe.left = left;
+    node->data.pipe.right = right;
+    return node;
+}
+
+void* Node_drop(Node *self)
+{
+    // TODO
+    return NULL;
+}
